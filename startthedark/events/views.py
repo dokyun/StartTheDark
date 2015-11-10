cb61836563a93015216a592e03fd43a474b4461c
Ubuntu 12.04.5 <dokyunjang@ubuntu.(none)>
2015-11-10 03:59:55 -0800

    7 .. ll doesn work

--- startthedark/events/views.py
+++ startthedark/events/views.py
@@ -9,15 +9,8 @@ from django.contrib.auth.decorators import login_required
 
 def tonight(request):
 	events = Event.objects.today().filter(latest=True)
-	attending = []
-	for event in events:
-		try:
-			Attendance.objects.get(event=event, user=request.user)
-			attending.append(True)
-		except Attenance.DoesNotExist:
-			attending.append(False)
 	context = {
-		'events': zip(events, attending),
+		'events': events,
 	}
 	return render_to_response(
 		'events/tonight.html',
@@ -25,6 +18,16 @@ def tonight(request):
 		context_instance = RequestContext(request),
 	)
 
+def archive(request):
+	events = Event.objects.filter(latest=True)
+	context = {
+		'events': events,
+	}
+	return render_to_response(
+		'events/archive.html',
+		context,
+		context_instance = RequestContext(request),
+	)
 
 def create(request):
 	form = EventForm(request.POST or None)
